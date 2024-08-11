b8d3fc68a1d942196b5889373e9faf0b296409e9
quincy mwangi <mwangiquincy77@gmail.com>
2024-08-11 19:58:32 +0300

    added gaming page json

--- src/pages/Gamingpage.js
+++ src/pages/Gamingpage.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import './Gamingpage.css'
+import { PRODUCTS } from './products'
+import { Product } from './Product'
 
 const Gamingpage = () => {
   return (
+    <>
     <div>
-        <h2>Hello</h2>
+        
     </div>
+    <div>Hello</div>
+    </>
   )
+  
 }
 
 export default Gamingpage
