cdd68216b09af10e173e1993273980557441fc8e
Frankline Were <franklinegift@gmail.com>
2024-08-12 15:29:00 +0300

    add hero section

--- src/pages/Library.js
+++ src/pages/Library.js
@@ -1,12 +1,21 @@
 import React from "react";
 
 import Navbar from "../Components/Navbar";
+import "./Library.css"
 
 function Library() {
   return (
     <>
-      <div>
+      <div id="nav">
         <Navbar />
+      </div>
+      <div id="hero-container">
+        <img src="/images/library-hero.jpg" alt="bookshelf" />
+        <div id="hero-content">
+        <h1>Isn't it hard to find <strong>Technological Books</strong>?</h1>
+        <p>worry not, for we went all out to give you just that!</p>
+        <button id="hero-button">Shop with us!</button>
+        </div>
       </div>
     </>
   );
