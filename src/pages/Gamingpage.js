6f1f6a654a5ae4fabe0628a1300b8d9fd746413a
quincy mwangi <mwangiquincy77@gmail.com>
2024-08-13 20:26:25 +0300

    update

--- src/pages/Gamingpage.js
+++ src/pages/Gamingpage.js
@@ -2,14 +2,21 @@ import React from 'react'
 import './Gamingpage.css'
 import { PRODUCTS } from './products'
 import { Product } from './Product'
+import Navbar from '../Components/Navbar'
 
 const Gamingpage = () => {
   return (
     <>
     <div>
-        
+        <Navbar/>
     </div>
-    <div>I am Quincy</div>
+    
+    <div className='products'>
+      {""}
+         {PRODUCTS.map((product)=>(
+          <Product data={product}/>
+         ))}
+    </div>
     </>
   )
   
