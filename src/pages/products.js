5771092391eaed1ea02ad851cbfcbf3ff895de3d
quincy mwangi <mwangiquincy77@gmail.com>
2024-08-13 20:30:41 +0300

    update

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,4 +1,4 @@
-/*import product1 from '../assets/images/product1.jpg';
+import product1 from '../assets/images/product1.jpg'
 import product2 from '../assets/images/product2.jpg';
 import product3 from '../assets/images/product3.jpg';
 import product4 from '../assets/images/product4.jpg';
@@ -7,46 +7,45 @@ import product6 from '../assets/images/product6.jpg';
 import product7 from '../assets/images/product7.jpg';
     export const PRODUCTS=[
         {
-            "id":1,
-            "productName":"Controller",
-            "Price":"1500",
-            "productImage":"product1"
+            id:1,
+            productName:"Controller",
+            Price:1500,
+            productImage:product1,
         },
         {
-            "id":2,
-            "productName":"Ps4",
-            "Price":"30,000",
-            "productImage":"/images/product2"
+            id:2,
+            productName:"Ps4",
+            Price:30000,
+            productImage:product2,
         },
         {
-            "id":3,
-            "productName":"Ps5",
-            "Price":"80,000",
-            "productImage":"/images/product3"
+            id:3,
+            productName:"Ps5",
+            Price:75000,
+            productImage:product3,
         },
         {
-            "id":4,
-            "productName":"XBox",
-            "Price":"60,000",
-            "productImage":"/images/product4"
+            id:4,
+            productName:"Xbox",
+            Price:60000,
+            productImage:product4,
+        }, 
+        {
+            id:5,
+            productName:"Pc",
+            Price:50000,
+            productImage:product5,
         },
         {
-            "id":5,
-            "productName":"Pc",
-            "Price":"65,000",
-            "productImage":"/images/product5"
+            id:6,
+            productName:"Ps2",
+            Price:15000,
+            productImage:product6,
         },
         {
-        "id":6,
-        "productName":"Ps2",
-        "Price":"15,000",
-        "productImage":"/images/product6"
+            id:7,
+            productName:"Game",
+            Price:2500,
+            productImage:product7,
         },
-        {
-            "id":7,
-            "productName":"Game",
-            "Price":"2500",
-            "productImage":"/images/product7"
-        }
     ]
-*/
