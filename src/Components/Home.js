33861a9316d02404cee28006323771de57c5e3b9
quincy mwangi <mwangiquincy77@gmail.com>
2024-08-14 13:15:41 +0300

    updates to food page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from './Navbar';
 import { Link } from 'react-router-dom';
 import './Home.css'
-impo
+
 
 //NB insert your page links in the page links div
 function Home() {
@@ -15,8 +15,9 @@ function Home() {
       
        
         <Link className='link' to='/Gaming'><img className='gamingpage' src='/gaming.jpg' alt=''/></Link>
-        <Link className='link' to='/Library'><img className='gamingpage' src='/images/library.png' alt=''/></Link>
-        <Link className='link' to='./Foodstuff'><img className='gamingpage' src='public/images/foodlogo.jpg' alt=''/></Link>
+        <Link className='link' to='/Library'><img className='gamingpage' src='./Pasted image.png' alt=''/></Link>
+        <Link className='link' to='/Kitchen'><img className='gamingpage' src='./kitchen equipments.jpg'/></Link>
+        <Link className='link' to='./Foodstuff'><img className='gamingpage' src='./food.jpg' alt=''/></Link>
         
      
         </div>
