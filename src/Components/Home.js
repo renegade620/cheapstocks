e19384b3c0c9de04863a68e9d7088efe0e05aafa
mutinduhh <jonathan.mutinda@student.moringaschool.com>
2024-08-14 14:42:53 +0300

    Helps

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,23 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Navbar';
 import { Link } from 'react-router-dom';
 import './Home.css'
-impo
+import Cart from './Cart';
+
 
 //NB insert your page links in the page links div
 function Home() {
+  const [cart,setCart] = useState([]);
+
+  const handleClick = (product)=>{
+   console.log(product)
+  }
   return (
     <>
     <div >
-      <Navbar/>
+      <Navbar size={Cart.length}/>
+      
        </div>
        <div className='page links'>
       
        
-        <Link className='link' to='/Gaming'><img className='gamingpage' src='/gaming.jpg' alt=''/></Link>
-        <Link className='link' to='/Library'><img className='gamingpage' src='/images/library.png' alt=''/></Link>
-        <Link className='link' to='./Foodstuff'><img className='gamingpage' src='public/images/foodlogo.jpg' alt=''/></Link>
-        
+
+
+        <Link handleClick={handleClick} className='link' to='/Gaming'><img className='gamingpage' src='./gaming.jpg' alt=''/></Link> 
+        <Link className='link' to='/Library'><img className='gamingpage' src='./Pasted image.png'/></Link>
+        <Link  to='/Kitchen'><img className='gamingpage link' src='./kitchen equipments.jpg'/></Link>
+        <Link  to=''><img className='gamingpage link' src='./foodlogo.jpg'/></Link>
+
      
         </div>
       </>
--- src/routes.js
+++ src/routes.js
@@ -5,9 +5,7 @@ import ErrorPage from './ErrorPage';
 import Home from './Components/Home';
 import Cart from './Components/Cart';
 import Library from './pages/Library';
-<<<<<<< HEAD
 import FoodStuffpage from './pages/FoodStuffPage';
-=======
 import Kitchen from './pages/Kitchen';
 
 
@@ -15,7 +13,6 @@ import Kitchen from './pages/Kitchen';
 
   const routes = [
    
->>>>>>> main
 
     {
         path: "/",
@@ -47,11 +44,6 @@ import Kitchen from './pages/Kitchen';
       element: <FoodStuffpage />,
       errorElement: <ErrorPage /> 
     }
-<<<<<<< HEAD
-=======
-    
-    
->>>>>>> main
   ];
 export default routes;
 
