9fb1a9c9d395bd81a547e7eae38f99142431ecd0
quincy mwangi <mwangiquincy77@gmail.com>
2024-08-14 23:59:44 +0300

    added about

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,8 +1,37 @@
-import React from 'react'
-import './About.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./About.css";
 
-export const About = () => {
+const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="about-container">
+      <h2>About CheapStocks</h2>
+      <p>
+        CheapStocks is your go-to platform for affordable stock .we are dedicated in providing best deals in wide range of product. 
+        we provide our goods at budget-friendly prices.our company started in 2021 after covid 19 pandemic.
+         It help people purchase goods online because of lock down and later grew up in to a bigger platform
+      </p>
+      <p>
+        Our mission is to democratize affordable stock  by offering a wide 
+        range of products across various categories, all at competitive prices.
+      </p>
+      <h3>Why Choose CheapStocks?</h3>
+      <ul>
+        <li>Affordable pricing</li>
+        <li>High-quality products</li>
+        <li>Wide variety of categories</li>
+        <li>Easy-to-use platform</li>
+        <li>Regular updates to our collection</li>
+      </ul>
+      <Link to="/cart">
+        <img 
+          className="cart"
+          src="/local_mall_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.png"
+          alt="Add to Cart" 
+        />
+      </Link>
+    </div>
+  );
+};
+
+export default About;
