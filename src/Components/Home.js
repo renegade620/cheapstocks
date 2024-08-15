63acab182ae44c78325cbab639d4388b70ef7ee2
quincy mwangi <mwangiquincy77@gmail.com>
2024-08-15 17:22:05 +0300

    main page design

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -14,21 +14,25 @@ function Home() {
   }
   return (
     <>
-   
+   <div className='body'>
     <div >
       <Navbar size={cart.length}/>
+       </div>
+       <div>
+        <img   className='description'src='/description.jpg' alt=''/>
        </div>
        <div className='page links'>
       
        
        <Link className='link' to='/Gaming'><img className='gamingpage' src='/gaming.jpg' alt=''/></Link>
         <Link className='link' to='/Library'><img className='gamingpage' src='./Pasted image.png' alt=''/></Link>
-        <Link className='link' to='/Kitchen'><img className='gamingpage' src='./kitchen equipments.jpg'/></Link>
+        <Link className='link' to='/Kitchen'><img className='gamingpage' src='./kitchen equipments.jpg' alt=''/></Link>
         <Link className='link' to='./FoodStuffPage'><img className='gamingpage' src='./foodlogo.jpg' alt=''/></Link>
         <Link className='link' to='./Toiletries'><img className='gamingpage' src='./TOIETRIES img.jpg' alt=''/></Link>
      </div>
      <div>
       <Footer/>
+     </div>
      </div>
       </>
       
